42520cd15795fb1ab4f710e86d96fb5e00af2480
Keemperor <jacobotau@hotmail.com>
2021-11-22 02:16:31 -0500

    generating correct answer loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,49 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerEl = document.getElementById('question-container');
-const shuffledQuestions, currentQuestionIndex
 const questionEl = document.getElementById('question');
-const answerButtonEl = document.getElementById('answer-button');
+const answerButtonEl = document.getElementById('answer-btns');
+
+let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
 
 function startGame () {
-console.log('started');
-startButton.classList.add('hide');
-shuffledQuestions = questions.sort(() => Math.random() - .5);
-currentQuestionIndex = 0;
-questionContainerEl.classList.remove('hide')
-setNextQuestion();
+  console.log('started');
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  questionContainerEl.classList.remove('hide');
+  setNextQuestion();
 };
 
 function setNextQuestion () {
+  resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]);
 };
 
 function showQuestion (question) {
-
+  questionEl.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    };
+    button.addEventListener('click', selectAnswer); 
+    answerButtonEl.appendChild(button);
+  });
 };
 
-function selectAnswer () {
+function resetState () {
+  nextButton.classList.add('hide');
+  while (answerButtonEl.firstChild) {
+    answerButtonEl.removeChild(answerButtonEl.firstChild);
+  };
+};
+
+function selectAnswer (e) {
 
 };
 
@@ -32,7 +52,7 @@ const questions = [
     question: 'What is 2 + 2',
     answers: [
       { text: '4', correct: true},
-      { text: '6', correct = false}
+      { text: '6', correct: false}
     ]
   }
 ];
