98b1d78cb05fa343caf320e3f7387600fab501a1
Keemperor <jacobotau@hotmail.com>
2021-11-22 02:50:56 -0500

    game logic complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ const answerButtonEl = document.getElementById('answer-btns');
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+})
 
 function startGame () {
   console.log('started');
@@ -44,14 +48,79 @@ function resetState () {
 };
 
 function selectAnswer (e) {
-
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+  }
+  else {
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
+  }
+  
 };
 
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+};
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
 const questions = [
+  {
+    question: 'Commonly used Data types DO Not include:',
+    answers: [
+      { text: 'Strings', correct: false},
+      { text: 'Booleans', correct: false},
+      { text: 'Alerts', correct: true},
+      { text: 'Numbers', correct: false}
+    ]
+  },
+  {
+    question: 'String values must be enclosed within',
+    answers: [
+      { text: '4', correct: true},
+      { text: '6', correct: false},
+      { text: '6', correct: false},
+      { text: '6', correct: false}
+    ]
+  },
   {
     question: 'What is 2 + 2',
     answers: [
       { text: '4', correct: true},
+      { text: '6', correct: false},
+      { text: '6', correct: false},
+      { text: '6', correct: false}
+    ]
+  },
+  {
+    question: 'What is 2 + 2',
+    answers: [
+      { text: '4', correct: true},
+      { text: '6', correct: false},
+      { text: '6', correct: false},
+      { text: '6', correct: false}
+    ]
+  },
+  {
+    question: 'What is 2 + 2',
+    answers: [
+      { text: '4', correct: true},
+      { text: '6', correct: false},
+      { text: '6', correct: false},
       { text: '6', correct: false}
     ]
   }
